851e2d8411bf5fc66c86a99885b844216994d4be
9449nxdmyv-tech <9449nxdmyv@privaterelay.appleid.com>
2025-10-28 13:42:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -42,12 +42,12 @@ import {
 // ============================================
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+      apiKey: "AIzaSyD2dBIb4rNczPQAxsyh-UkKSJrp0gLrnKA",
+      authDomain: "art-drops-production.firebaseapp.com",
+      projectId: "art-drops-production",
+      storageBucket: "art-drops-production.firebasestorage.app",
+      messagingSenderId: "738166123505",
+      appId: "1:738166123505:web:2e938dc32c436bcd693250"
 };
 
 // Initialize Firebase
