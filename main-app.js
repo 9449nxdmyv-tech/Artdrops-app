3f0bb840ab5a3afbbf432200d24b48e26d111be5
9449nxdmyv-tech <9449nxdmyv@privaterelay.appleid.com>
2025-10-29 13:47:52 -0500

    Update main-app.js

--- main-app.js
+++ main-app.js
@@ -2,7 +2,6 @@
 // MAIN APP - Entry Point
 // ============================================
 
-
 // Import all services and modules
 import { auth, db, storage } from '/services/firebase-config.js';
 import { authService } from '/services/auth-service.js';
